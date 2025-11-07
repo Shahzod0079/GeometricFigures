5f4b0fc18c6567a1711f1af0572f93c91aac61c2
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-07 22:20:24 +0500

    Добавлен класс Circle

---
+++ Classes/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace GeometricFigures
+{
+    public class Circle : GeometricFigure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double centerX, double centerY, double radius)
+            : base(centerX, centerY)
+        {
+            Radius = radius;
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            var ellipse = new Ellipse
+            {
+                Width = Radius * 2,
+                Height = Radius * 2,
+                Fill = Brushes.Red,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+
+            Canvas.SetLeft(ellipse, CenterX - Radius);
+            Canvas.SetTop(ellipse, CenterY - Radius);
+
+            canvas.Children.Add(ellipse);
+        }
+    }
+}
