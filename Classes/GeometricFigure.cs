64b9d357e6c9e91ea965a9309a606455b61ee7cc
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-07 22:14:36 +0500

    Добавлен базовый класс GeometricFigure

---
+++ Classes/GeometricFigure.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace GeometricFigures
+{
+    public class GeometricFigure
+    {
+        public double CenterX { get; set; }
+        public double CenterY { get; set; }
+
+        public GeometricFigure(double centerX, double centerY)
+        {
+            CenterX = centerX;
+            CenterY = centerY;
+        }
+
+        public virtual void Draw(Canvas canvas)
+        {
+            // Базовая реализация будет в наследниках
+        }
+    }
+}
