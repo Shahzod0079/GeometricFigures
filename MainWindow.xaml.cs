13c9f5788e31818bd24f3e5b713e5d48c4395d72
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-07 22:27:47 +0500

    Обновленный MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace GeometricFigures
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем фигуры
+            var triangle = new Triangle(100, 100, 80);
+            var circle = new Circle(250, 150, 50);
+            var triangle2 = new Triangle(400, 200, 60);
+            var circle2 = new Circle(150, 300, 40);
+
+            // Рисуем фигуры на Canvas
+            triangle.Draw(MainCanvas);
+            circle.Draw(MainCanvas);
+            triangle2.Draw(MainCanvas);
+            circle2.Draw(MainCanvas);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Очищаем Canvas
+            MainCanvas.Children.Clear();
         }
     }
 }
